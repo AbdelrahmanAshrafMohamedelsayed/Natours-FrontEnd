a8a31cb7c26398a00438da3506622d2565a67969
AbdelrahmanAshrafMohamedelsayed <97232730+AbdelrahmanAshrafMohamedelsayed@users.noreply.github.com>
2023-10-16 03:42:36 +0300

    Feat

--- src/Features/index.js
+++ src/Features/index.js
@@ -1,5 +1,5 @@
 import LeftSection from "./LeftSection";
 import Reviews from "./reviews";
 import Map2 from "./Map";
-import RightSection from "./RightSection"; // change the import statement to use the correct casing
-export { LeftSection, RightSection, Reviews, Map2 };
+import rightSection from "./rightSection"; // change the import statement to use the correct casing
+export { LeftSection, rightSection as RightSection, Reviews, Map2 };
